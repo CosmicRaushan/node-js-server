686cbdd658e8283e95788f29eb614b33f1fea01d
CosmicRaushan <raushang804420@gmail.com>
2026-06-07 13:58:49 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app =  express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Hello Wrold from the production level code.."
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on http://localhost:${PORT}`)
+})
